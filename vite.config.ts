18a1990aa9b15d0f35c90a9774e75c0438d82668
vivek07777777 <abhishekkumarsingh841441@gmail.com>
2026-06-15 16:45:14 +0530

    added favicon

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,6 @@ export default defineConfig(({mode}) => {
       // HMR is disabled in AI Studio via DISABLE_HMR env var.
       // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
       hmr: process.env.DISABLE_HMR !== 'true',
-      allowedHosts : ["222b-103-59-207-242.ngrok-free.app"]
     },
   };
 });
